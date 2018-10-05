3869623eece1075463ad0bf0e88ef24264b013ab
Joel Will <joel-willb@hotmail.com>
2018-10-04 22:06:19 -0300

    Recursivo

--- train_data.py
+++ train_data.py
@@ -3,7 +3,9 @@ import os
 import os.path
 import shutil
 import numpy as np
+import time
 from pymongo import MongoClient
+from random import randint
 from face_recognition.face_recognition_cli import image_files_in_folder
 
 def get_train_data(train_dir,verbose=False):
@@ -27,13 +29,17 @@ def get_train_data(train_dir,verbose=False):
     banco = cliente.face_encodings
 
     group = banco.group
-    person_group = group.find({person_group})
+
+    for doc in group.find():
+        print(doc)
+
+    person_group = {}
 
     # X é uma lista de 'encodings' dos rostos das pessoas
     # Y é uma lista com o nome das pessoas
     # 
 
-    #X, y = get_persons_encondings(banco.group, [], [])
+    #X, y = get_persons_encondings(banco.group.find(), [], [])
 
     X, y = [],[]
 
@@ -48,7 +54,7 @@ def get_train_data(train_dir,verbose=False):
     
     # Passa por todas a pessoas
     for class_dir in os.listdir(train_dir):
-        person = person_group.person
+        person = {}
 
         #Se não for um diretório pula para o próximo loop
         if not os.path.isdir(os.path.join(train_dir, class_dir)):
@@ -71,18 +77,18 @@ def get_train_data(train_dir,verbose=False):
                 # Faz um 'enconding da face e adiciona na lista de faces'
                 enconding = face_recognition.face_encodings(image, known_face_locations=face_bounding_boxes)[0]
 
-                document = {
-                    "X": enconding.tolist()
-                }
-
-                person.insert_one(document)
+                person[str(int(time.time()))] = enconding.tolist()
 
                 X.append(enconding)
                 # Adiciona o nome da pessoa na lista de nome de pessoas
                 y.append(class_dir)
 
+        person_group[class_dir] = person
+
         #Apaga a pasta quando terminar de processa-lá
         shutil.rmtree(train_dir+"/"+class_dir)
+
+    group.insert_one(person_group)
 
     # Salva a lista em arquivos
     '''
@@ -92,14 +98,66 @@ def get_train_data(train_dir,verbose=False):
     
     return X, y
 
-def get_persons_encondings(collection, X, y):
-    if len(collection.collection_names()) > 0: # É uma coleção
-        for subcollection in collection.collection_names():
+def get_data(parent, collection, verbose):
+    path = os.path.join(parent, collection)
+    
+    collection_dict = {}
+    X, y = [], []
+    if os.path.isdir(path):
+
+        for subcollection in os.listdir(path):
+            sub_dict, sub_x, sub_y = get_data(path, subcollection, verbose)
+
+            X, y = X + sub_x, y + sub_y
+
+            if (len(sub_dict) == 0): 
+                collection_dict, sub_x, sub_y = get_person_encondings(collection,path,verbose)
+                X, y = X + sub_x, y + sub_y
+                break
+            
+            else:
+                collection_dict[subcollection] = sub_dict
+        
+        if (parent != ""): 
+            return collection_dict, X, y
+        else:
+            return {"person_group":collection_dict}, X, y
+    
+    else:
+        return {}, [], []
+
+def get_person_encondings(collection,path, verbose):
+    doc = []
+    X = []
+    for img_path in image_files_in_folder(path):
+        # Carrega a imagem, descobre as faces e seus limites 
+        # Ex: [(x1,y1,w1,h1), (x2,y2,w2,h2), ...]
+        image = face_recognition.load_image_file(img_path)
+        face_bounding_boxes = face_recognition.face_locations(image)
+
+        # Verifica se a quantidade de faces reconhecidas foi diferente de 1
+        if len(face_bounding_boxes) != 1:
+            # Ignora imagem e se verbose = true, faz um print
+            if verbose:
+                print("Image {} not suitable for training: {}".format(img_path, "Didn't find a face" if len(face_bounding_boxes) < 1 else "Found more than one face"))
+        else:
+            # Faz um 'enconding da face e adiciona na lista de faces'
+            enconding = face_recognition.face_encodings(image, known_face_locations=face_bounding_boxes)[0]
+
+            doc.append(enconding.tolist())
+
+            X.append(enconding)
+
+    return doc, X, [collection]  * len(X)
+
+def get_persons_encondings(doc , X, y):
+    if len(doc.collection_names()) > 0: # É uma coleção
+        for subcollection in doc.collection_names():
             X, y = get_persons_encondings(subcollection, X, y)
     else:
-        documents = get_documents_data(collection)
+        documents = get_documents_data(doc)
 
-    return X + documents, y + [collection * len(documents)]
+    return X + documents, y + [doc * len(documents)]
 
 def get_documents_data(collection):
     X = []
